7664893d8abec936765152e484a71c0d34bcb133
Joseff Santiago <santia76@students.rowan.edu>
2023-02-09 09:40:46 -0500

    No changes
    just wanted to clear the notification, it wouldnt go away. pretty sure nothing changed

--- 1 Buttons and Pullup PullDown Resistors/Part1.c
+++ 1 Buttons and Pullup PullDown Resistors/Part1.c
@@ -16,9 +16,6 @@
 
 void gpioInit();
 
-/**
- * main.c
- */
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
@@ -32,9 +29,9 @@ int main(void)
 
     while(1)
     {
-        if(~P2IN & BIT3){            // If S2 (P2.3) is pressed
+        if(~P2IN & BIT3){           // If S2 (P2.3) is pressed
             P6OUT ^= BIT6;}         // pause the blinking on P6.6
-        if(~P4IN & BIT1){            // If S1 (P4.1) is pressed
+        if(~P4IN & BIT1){           // If S1 (P4.1) is pressed
             P1OUT ^= BIT0;}         // pause the blinking P1.0
         __delay_cycles(250000);     // Delay for 100000*(1/MCLK)=0.1s
     }
