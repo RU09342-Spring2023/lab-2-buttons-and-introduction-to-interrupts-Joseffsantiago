0f69f382fc5b399a069f3d3c0b0ef2df6a9c2e2c
Joseff Santiago <santia76@students.rowan.edu>
2023-02-09 19:06:08 -0500

    Maybe Better
    made the clauses inside the if statements more strict

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -17,19 +17,19 @@ int main() {
     PM5CTL0 &= ~LOCKLPM5;       //not sure what this does but it was in every other thing so i put it here
 
     while(1){
-            if(P4IN&BIT1){
+            if((P4IN&BIT1)&(i<=9)){
                 state=2;
                 i++;
                 __delay_cycles(1000000);
             }
-            else if(i>=10){
+            else if((P4IN&BIT1)&(i>=10)){
                 state=3;
             }
-            else{state==1;}
-      switch(state)
+            else {state=1;}
+      switch(state){
       case 1: P1OUT ^= BIT0; P6OUT &=~BIT6; __delay_cycles(500000); break;
       case 2: P1OUT &=~BIT0; P6OUT ^= BIT6; __delay_cycles(250000); break;
-      case 3: P1OUT &=~BIT0; P6OUT |= BIT6; break;
+      case 3: P1OUT &=~BIT0; P6OUT |= BIT6; break;}
     }
 }
 
